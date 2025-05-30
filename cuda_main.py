27bda991c979e48b2b188ddb99842112e115315c
Stanley <stanleyyin3@gmail.com>
2025-05-30 03:30:07 -0400

    93% faster training

---
+++ cuda_main.py
@@ -0,0 +1,120 @@
+import cupy as cp
+import pandas as pd
+from matplotlib import pyplot as plt
+
+NUMBER_OF_EPOCHS = 5000
+
+def init_params():
+    W1 = cp.random.rand(10, 784, dtype=cp.float32) - 0.5
+    b1 = cp.random.rand(10, 1, dtype=cp.float32) - 0.5
+    W2 = cp.random.rand(10, 10, dtype=cp.float32) - 0.5
+    b2 = cp.random.rand(10, 1, dtype=cp.float32) - 0.5
+    return W1, b1, W2, b2
+
+def ReLU(Z):
+    return cp.maximum(0, Z)
+
+def softmax(Z):
+    Z_shift = Z - cp.max(Z, axis=0, keepdims=True)
+    expZ    = cp.exp(Z_shift)
+    return expZ / cp.sum(expZ, axis=0, keepdims=True)
+
+def forward_prop(W1, b1, W2, b2, X):
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def one_hot(Y):
+    one_hot_Y = cp.zeros((Y.size, int(Y.max()) + 1), dtype=cp.float32)
+    one_hot_Y[cp.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def deriv_ReLU(Z):
+    return Z > 0
+
+def back_prop(Z1, A1, Z2, A2, W2, X, Y):
+    m = Y.size
+    one_hot_Y = one_hot(Y)
+    
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * cp.sum(dZ2, axis=1, keepdims=True)
+    
+    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * cp.sum(dZ1, axis=1, keepdims=True)
+    
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return cp.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return cp.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, epochs, alpha):
+    W1, b1, W2, b2 = init_params()
+    for i in range(epochs):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 10 == 0:
+            print("Epoch: ", i)
+            print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
+    return W1, b1, W2, b2
+
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+def test_prediction(X_train, Y_train, index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    img_cpu = cp.asnumpy(current_image).reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(img_cpu, interpolation='nearest')
+    plt.show()
+
+def main():
+    data = pd.read_csv("train.csv")
+
+    data.head()
+
+    data = cp.array(data, dtype=cp.float32)
+    m, n = data.shape
+    print(m, n)
+    cp.random.shuffle(data)
+
+    data_dev = data[0:1000].T
+    Y_dev = data_dev[0].astype(cp.int32)
+    X_dev = data_dev[1:n]
+
+    data_train = data[1000:m].T
+    Y_train = data_train[0].astype(cp.int32)
+    X_train = data_train[1:n].astype(cp.float32)
+    X_train = X_train / 255.0
+
+    W1, b1, W2, b2 = gradient_descent(X_train, Y_train, NUMBER_OF_EPOCHS, 0.1)
+
+    test_prediction(X_train, Y_train, 6, W1, b1, W2, b2)
+    dev_predictions = make_predictions(X_dev, W1, b1, W2, b2)
+    get_accuracy(dev_predictions, Y_dev)
+
+if __name__ == "__main__":
+    main()
